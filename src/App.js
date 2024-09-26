c3ab15d186e6eae31bd79da234e6f65d773addff
김욱진 <dnrwls9115@naver.com>
2024-09-27 02:48:31 +0900

    프론트뼈대완, 글목록,글상세,이미지등디자인

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Login from './pages/Login.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './pages/Signup.js';
 import Profile from './pages/Profile.js'
+import CreateDiary from './pages/CreateDiary.js';
+import DiaryDetail from './pages/DiaryDetail.js'
+import DiaryList  from './pages/DiaryList.js';
 function App() {
   return ( 
     <Router>
@@ -14,6 +17,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/creatediary" element={<CreateDiary />} />
+        <Route path="/diarydetail" element={<DiaryDetail />} />
+        <Route path="/diarylist" element={<DiaryList />} />
       </Routes>
     </Router>
 
